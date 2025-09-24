3b26df15a77a13457219af5df513e6684fe563f6
anuj sharma <anujsharma2928@gmail.com>
2025-09-24 19:41:37 +0530

    cors frontend url fixed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://feasto-p6y1.onrender.com"],
     credentials:true,
     methods:['POST','GET']
 }
